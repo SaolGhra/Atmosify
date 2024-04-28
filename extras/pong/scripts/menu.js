525b62bf66d62854be6dc5a6ef67b862e3cee376
SaolGhra <DeathCRystalYT@Gmail.com>
2024-04-28 22:25:10 +0100

    V1.6.7
    Removed the aivai from menu for time being until i have time to fix it

--- extras/pong/scripts/menu.js
+++ extras/pong/scripts/menu.js
@@ -1,19 +1,9 @@
-import { startAIVsAI } from "./gameAIVAI.js";
 import { start1v1 } from "./game1v1.js";
 import { start1vAI } from "./game1vai.js";
 
 let isGamePaused = false;
+let currentGameMode = null;
 
-// Function to start the selected game mode
-function startGame(startFunction) {
-  // Hide menu and show game
-  hideMenu();
-
-  // Start the selected game mode
-  startFunction();
-}
-
-// Event listeners for game mode buttons
 document.getElementById("start1v1Button").addEventListener("click", () => {
   startGame(start1v1);
 });
@@ -22,18 +12,13 @@ document.getElementById("startAIButton").addEventListener("click", () => {
   startGame(start1vAI);
 });
 
-document.getElementById("backtomenu").addEventListener("click", () => {
-  showMenu();
-});
+function startGame(gameMode) {
+  hideMenu();
+  gameMode();
+}
 
-const pauseButton = document.querySelector("#pauseButton");
-
-pauseButton.addEventListener("click", () => {
-  if (isGamePaused) {
-    resumeGame();
-  } else {
-    pauseGame();
-  }
+document.getElementById("pauseButton").addEventListener("click", () => {
+  location.reload();
 });
 
 const cover = document.querySelector("#cover");
@@ -42,25 +27,32 @@ const menu = document.querySelector(".start-menu");
 function hideMenu() {
   cover.style.display = "none";
   menu.style.display = "none";
-  pauseButton.style.display = "block";
-}
-
-function showMenu() {
-  cover.style.backgroundImage = "url('" + startAIVsAI() + "')"; // Set background image URL correctly
-
-  cover.style.display = "block";
-  menu.style.display = "flex";
-  pauseButton.style.display = "none";
 }
 
 // Pause Game
-function pauseGame() {
-  isGamePaused = true;
-  showMenu();
-}
+// function pauseGame() {
+//   isGamePaused = true;
+//   if (currentGameMode) {
+//     currentGameMode.pause();
+//   }
+// }
 
-// Resume Game
-function resumeGame() {
-  isGamePaused = false;
-  hideMenu(); // Hide menu when resuming the game
+// // Resume Game
+// function resumeGame() {
+//   isGamePaused = false;
+//   if (currentGameMode) {
+//     currentGameMode.resume();
+//   }
+// }
+
+const pauseButton = document.querySelector("#pauseButton");
+
+pauseButton.addEventListener("click", () => {
+  location.reload();
+  showMenu();
+});
+
+function showMenu() {
+  cover.style.display = "block";
+  menu.style.display = "flex";
 }
